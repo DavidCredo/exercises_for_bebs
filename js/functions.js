a8205fcb24fa3a376e0a61dcc4aeecb0a9d9bd8e
DavidC <davidcredo00@gmail.com>
2022-08-19 20:54:29 +0200

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+// Hier sind ein paar Aufgaben zu Funktionen in Javascript.
+
+// 1. Schreibe eine Funktion, die als Parameter eine Zahl übergeben bekommt (Minuten) und diese in Sekunden konvertiert und zurückgibt.
+// Die Ausgabe soll ungefähr so aussehen = convertToSeconds(3) // loggt in die Konsole 180.
+
+//	console.log(convertToSeconds(3));
+
+// 2. Wenn eine negative Zahl übergeben wird, soll die Funktion die Eingabe automatisch "normalisieren" also positiv darstellen.
+//	console.log(convertToSeconds(-20));
+
+
