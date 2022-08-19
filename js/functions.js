39d8369254624ff6df091bdf72058f6451f34feb
DavidC <davidcredo00@gmail.com>
2022-08-19 21:14:27 +0200

    added practice problems to js

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,22 @@
 // Hier sind ein paar Aufgaben zu Funktionen in Javascript.
 
 // 1. Schreibe eine Funktion, die als Parameter eine Zahl übergeben bekommt (Minuten) und diese in Sekunden konvertiert und zurückgibt.
-// Die Ausgabe soll ungefähr so aussehen = convertToSeconds(3) // loggt in die Konsole 180.
 
-//	console.log(convertToSeconds(3));
+//console.log(convertToSeconds(3));
 
 // 2. Wenn eine negative Zahl übergeben wird, soll die Funktion die Eingabe automatisch "normalisieren" also positiv darstellen.
-//	console.log(convertToSeconds(-20));
 
+//console.log(convertToSeconds(-20));
 
+// 3. Schreibe eine Funktion returnFirstElement(array), die einen Array als Parameter übergeben bekommt (Egal ob int, Strings, ...)
+// und das erste Element im Array zurückgibt.
+
+// const arr = [1, 2, 4, 5, 23];
+// console.log(returnFirstElement(arr));
+
+// 4. Schreibe eine Funktion isInArray(num, array), die eine Zahl und einen Array mit Zahlen als Parameter übergeben bekommt.
+// Die FUnktion soll prüfen, ob die Zahl im Array vorkommt und true oder false zurückgeben.
+
+// const number = 12;
+// const array = [15, 14, 44, 41, 42, 12];
+// console.log(isInArray(number, array));
